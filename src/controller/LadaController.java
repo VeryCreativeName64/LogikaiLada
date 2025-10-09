dce372a8868b2c38bd19c2dc58fd74a335cd4d08
timu785 <sajtos.timudzsin.gergo@diak.szamalk-szalezi.hu>
2025-10-09 13:00:41 +0200

    működő gombok

--- src/controller/LadaController.java
+++ src/controller/LadaController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import modell.LadaModell;
 import view.LadaView;
 
@@ -10,5 +13,41 @@ public class LadaController {
         LadaModell ladaModel3 = new LadaModell("bronz", false);
         LadaView view = new LadaView();
         view.setVisible(true);
+        
+        JButton aranyGomb = view.getButtonArany();
+        aranyGomb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ladaModel1.isBennevan()){
+                    view.setValasz("Gratulálunk, ebben a ládában van!");  
+                } else {
+                    view.setValasz("Nem ebben a ládában van!");
+                }
+            }
+        });
+        
+        JButton ezustGomb = view.getButtonEzust();
+        ezustGomb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ladaModel2.isBennevan()){
+                    view.setValasz("Gratulálunk, ebben a ládában van!");  
+                } else {
+                    view.setValasz("Nem ebben a ládában van!");
+                }
+            }
+        });
+        
+        JButton bronzGomb = view.getButtonBronz();
+        bronzGomb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(ladaModel3.isBennevan()){
+                    view.setValasz("Gratulálunk, ebben a ládában van!");  
+                } else {
+                    view.setValasz("Nem ebben a ládában van!");
+                }
+            }
+        });
     }
 }
