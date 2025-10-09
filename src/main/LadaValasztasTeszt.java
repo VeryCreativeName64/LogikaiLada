7ebf854957e9975d83b5277be11b18dc021f5491
DezsoV <2005bnm@gmail.com>
2025-10-09 13:55:44 +0200

    tesztalapok elkeszitve

---
+++ src/main/LadaValasztasTeszt.java
@@ -0,0 +1,31 @@
+package main;
+
+public class LadaValasztasTeszt {
+    public void tesztModellLadakFeliratVanE(){
+        
+    }
+    
+    public void tesztModellLadakFeliratai(){
+        
+    }
+    
+    public void tesztModell3Lada1Kincs(){
+        
+    }
+    
+    public void tesztModellNemLetezoLada(){
+        
+    }
+    
+    public void tesztVezerloLadaTartalomVisszajelzes(){
+        
+    }
+    
+    public void tesztVezerloGyozelemVisszajelzes(){
+        
+    }
+    
+    public void tesztVezerloSzovegSzamKezeles(){
+        
+    }
+}
