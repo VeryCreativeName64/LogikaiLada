b75d338b14f245dc856d4b9cd6a470d27b7ef784
DezsoV <2005bnm@gmail.com>
2025-10-09 13:10:28 +0200

    uj jatek gomb

--- src/controller/LadaController.java
+++ src/controller/LadaController.java
@@ -1,14 +1,48 @@
 package controller;
 
+import javax.swing.JOptionPane;
 import modell.LadaModell;
 import view.LadaView;
 
 public class LadaController {
+    private LadaModell ladaModellArany;
+    private LadaModell ladaModellEzust;
+    private LadaModell ladaModellBronz;
+    private LadaView nezet;
+
     public LadaController() {
-        LadaModell ladaModel1 = new LadaModell("arany", false);
-        LadaModell ladaModel2 = new LadaModell("ezüst", true);
-        LadaModell ladaModel3 = new LadaModell("bronz", false);
-        LadaView view = new LadaView();
-        view.setVisible(true);
+        ladaModellArany = new LadaModell("arany", false);
+        ladaModellEzust = new LadaModell("ezüst", true);
+        ladaModellBronz = new LadaModell("bronz", false);
+        nezet = new LadaView();
+        nezet.getButtonArany().addActionListener(e -> valasztas(nezet, "Rossz válasz"));
+        nezet.getButtonEzust().addActionListener(e -> valasztas(nezet, "Jó válasz"));
+        nezet.getButtonBronz().addActionListener(e -> valasztas(nezet, "Rossz válasz"));
+        nezet.setVisible(true);
+    }
+
+    private void valasztas(LadaView view, String message) {
+        JOptionPane.showMessageDialog(view, message, "Eredmény", JOptionPane.INFORMATION_MESSAGE);
+        view.setGombokEngedelyezve(false);
+        int valasz = JOptionPane.showOptionDialog(
+            view,
+            "Szeretnél új játékot?",
+            "Vége",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            new Object[]{"Új játék", "Kilépés"},
+            "Új játék"
+        );
+        if (valasz == JOptionPane.YES_OPTION) {
+            ujJatek();
+        } else {
+            view.dispose();
+        }
+    }
+
+    private void ujJatek(){
+        nezet.setGombokEngedelyezve(true);
+        nezet.torolValasz();
     }
 }
